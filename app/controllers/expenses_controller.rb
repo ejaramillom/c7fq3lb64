e54ad761df9317773f0580c9c4760d20208eccfd
ejaramillom <ejaramilo@hotmail.com>
2019-11-03 16:11:18 -0500

    fixed params filters

--- app/controllers/expenses_controller.rb
+++ app/controllers/expenses_controller.rb
@@ -1,5 +1,43 @@
 class ExpensesController < ApplicationController
   def index
-     @expenses = Expense.order("date DESC")
+
+    @expenses = Expense.order("date DESC")
+
+    if params[:category_id] == ""
+      params[:category_id] = nil
+    end
+    if params[:concept] == ""
+      params[:concept] = nil
+    end
+
+    if params[:concept] && params[:category_id]
+      @expenses = Expense.where(:concept => params[:concept]).where(:category_id => params[:category_id])
+    end
+    if params[:concept]
+      @expenses = Expense.where(:concept => params[:concept])
+    end
+    if params[:concept] == "" and params[:category_id]
+      @expenses = Expense.where(:category_id => params[:category_id])
+    end
+    if params[:category_id]
+      @expenses = Expense.where(:category_id => params[:category_id])
+    end
+    if params[:concept] == "" and params[:category_id] == ""
+      @expenses = Expense.order("date DESC")
+    end
+
+    # @expenses = Expense.order("date DESC")
+    # @expenses = Expense.where(:concept => text_field_tag)
+    # @search = params[:expense]
   end
 end
+
+# if params[:concept] and params[:category_id]
+#   @expenses = Expense.where(:concept => params[:concept]).where(:category_id => params[:category_id])
+# elsif params[:concept] and params[:category_id] == nil
+#   @expenses = Expense.where(:concept => params[:concept])
+# elsif params[:concept] == nil and params[:category_id]
+#   @expenses = Expense.where(:category_id => params[:category_id])
+# else
+#   @expenses = Expense.order("date DESC")
+# end
